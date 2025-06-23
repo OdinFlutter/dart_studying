228f12b8ab8cd8ef92b32a5510fd16b3a92fa69f
O Irtyshchev <citrus29042015@icloud.com>
2025-06-23 12:11:53 +0300

    ded first task

--- 1_task.dart
+++ 1_task.dart
@@ -1,3 +1,20 @@
 void main(){
+  runAllTasks();
+}
 
+void runAllTasks(){
+  firstTask();
+}
+
+void firstTask(){
+  double width = 18.4;
+  double height = 30.9;
+
+  double perimeter = (width + height) * 2;
+  double area = width * height;
+  int squares = area ~/ 1;
+
+  print('Периметр прямокутника: ${perimeter}');
+  print('Площа прямокутника: ${area}');
+  print('Повних квадратів поміститься: ${squares}');
 }
